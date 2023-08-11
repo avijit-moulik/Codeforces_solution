e083555fb51469cdd788e46e91b811a63041cf89
Avijit Moulik <137809579+avijit-moulik@users.noreply.github.com>
2023-08-11 23:55:29 +0600

    Create 486A - Calculating Function.cpp

---
+++ 486A - Calculating Function.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+
+
+using namespace std;
+
+int main()
+{
+   long long n;
+   cin >> n;
+   if (n%2==0)
+   {
+		cout << n/2;
+   }
+   else
+   cout << ((n/2)+1)*-1;
+  
+   
+}
