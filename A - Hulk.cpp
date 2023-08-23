bccc0b0f8f6f8d13e2054ac774361813c6b5b883
Avijit Moulik <137809579+avijit-moulik@users.noreply.github.com>
2023-08-23 15:28:14 +0600

    Create A - Hulk.cpp

---
+++ A - Hulk.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+using namespace std;
+int main()
+{
+  int n, a{1};
+  string x, y;
+
+  cin >> n;
+  cout << "I hate ";
+  if (n > 1)
+  {
+    for (int i = 0; i < n-1; i++)
+    {
+      cout << " that ";
+      if (a % 2 != 0)
+      {
+        cout << " I love ";
+      }
+      else
+      {
+        cout << " I hate ";
+      }
+      a++;
+    }
+  }
+
+  cout << "it";
+}
