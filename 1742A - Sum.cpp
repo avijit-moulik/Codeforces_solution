f565d072283d451b97616681d45dc75e9d2140c0
Avijit Moulik <137809579+avijit-moulik@users.noreply.github.com>
2023-09-12 22:02:11 +0600

    Create 1742A - Sum.cpp

---
+++ 1742A - Sum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n, a, b, c;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b >> c;
+        if (a + b == c || a + c == b || b + c == a)
+    {
+        cout << "YES\n";
+    }
+    else
+        cout << "NO\n";
+    }
+    
+}
