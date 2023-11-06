78dcc582f28d3446aaecc36374eff6e7b3164240
Avijit Moulik <137809579+avijit-moulik@users.noreply.github.com>
2023-11-07 00:43:38 +0600

    266B - Queue at the School.cpp

---
+++ 266B - Queue at the School.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, t;
+	string s;
+	cin >> n >> t >> s;
+	for (int i = 0; i < t; i++)
+
+	{
+		for (int i = 1; i < n; ++i)
+		{
+			if (s[i] == 'G' && s[i - 1] == 'B')
+			{
+				s[i] = 'B';
+				s[i - 1] = 'G';
+				i += 1;
+			}
+		}
+	}
+	cout << s;
+}
