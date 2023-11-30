5353aa76c8c8f0a59bfe23fb0f841ce763ed9f99
Avijit Moulik <137809579+avijit-moulik@users.noreply.github.com>
2023-12-01 00:51:37 +0600

    C - Theofanis' Nightmare .cpp

---
+++ C - Theofanis' Nightmare .cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+#include<cctype>
+#define arrayInput long long int n; cin >> n; long long int a[n]; for(int i=0; i<n; i++) cin >> a[i];
+#define test int t; cin >> t; while(t--)
+#define iloop(a, n) for(int i=a; i<n; i++)
+#define jloop(a, n) for(int j=a; j<n; j++)
+#define dloop(a, n) for(int i=a; i>n; i--)
+#define print(a) cout << a << "\n";
+#define dprint(a, b) cout << a << " " << b << endl;
+#define nd &&
+#define rr ||
+#define ek ==
+#define nek !=
+#define ll long long
+#define lli long long int
+#define asort(a, n) sort(a, a+n);
+#define dsort(a, n) sort(a,a+n,greater<int>())
+#define yesPrint cout << "YES" << endl;
+#define noPrint cout << "NO" << endl;
+#define dPrint(a, b) cout << a << " " << b << "\n";
+#define aeprint(a) cout << a << " ";
+#define line cout << "\n";
+#define maxArray(a,n) *max_element(a, a+n)
+#define minArray(a,n) *min_element(a, a+n)
+#define fast  ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize // vectors
+#define ins insert // oops size(x), rbegin(x), rend(x)
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+
+// #define int long long
+#define pb push_back
+#define ph push_heap
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i) // loops
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+#define forn(i, x, n) for (int i = x; i < n; i++)
+#define formi(i, x, n) for (int i = x; i < n; i--)
+
+using namespace std;
+
+
+int mf(int a, int b)
+{
+    while (true)
+    {
+        if (a == 0) return b;
+        b %= a;
+        if (b == 0) return a;
+        a %= b;
+    }
+}
+
+int lcm(int a, int b)
+{
+    int temp = mf(a, b);
+
+    return temp ? (a / temp * b) : 0;
+}
+
+void solve(){
+    ll n;
+        cin >> n;
+        ll a[n];
+        iloop(0, n)
+            cin >> a[i];
+
+        ll rem = 0;
+        pair<ll, ll> pp[n + 1];
+        pp[n] = {0, 1};
+
+        for(int i = n - 1; i >= 0; i--){
+            ll to = pp[i + 1].first;
+            ll c = pp[i + 1].second;
+            pp[i] = {to + a[i], c};
+            pp[i] = max(pp[i], {to + rem + a[i], c + 1});
+            rem += a[i];
+        }
+
+        print(pp[0].first);
+    }
+
+
+int main(){
+    fast; 
+    test{
+        solve();
+    }
+
+ 
+}
