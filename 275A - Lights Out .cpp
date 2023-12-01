91828a238c778c898a9318397f1b39083b845f95
Avijit Moulik <137809579+avijit-moulik@users.noreply.github.com>
2023-12-01 21:23:01 +0600

    275A - Lights Out .cpp

---
+++ 275A - Lights Out .cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+#include<cctype>
+#define arrayInput long long int n; cin >> n; long long int a[n]; for(int i=0; i<n; i++) cin >> a[i];
+#define test int t; cin >> t; while(t--)
+#define print(a) cout << a << "\n";
+#define dprint(a, b) cout << a << " " << b << endl;
+#define nd &&
+#define rr ||
+#define ek ==
+#define nek !=
+#define lli long long int
+#define asort(a, n) sort(a, a+n);
+#define dsort(a, n) sort(a,a+n,greater<int>())
+#define yesPrint cout << "YES" << endl;
+#define noPrint cout << "NO" << endl;
+#define dPrint(a, b) cout << a << " " << b << "\n";
+#define aeprint(a) cout << a << " ";
+#define line cout << "\n";
+#define maxArray(a,n) *max_element(a, a+n)
+#define minArray(a,n) *min_element(a, a+n)
+#define fast  ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize // vectors
+#define ins insert // oops size(x), rbegin(x), rend(x)
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+
+// #define int long long
+#define pb push_back
+#define ph push_heap
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i) 
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+#define forn(i, x, n) for (int i = x; i < n; i++)
+#define formi(i, x, n) for (int i = x; i < n; i--)
+using ll = long long;
+using db = long double; // or double,
+
+
+#define vi vector<int>
+#define vpp vector<pair<int, int>>
+#define vs vector<string>
+#define vll vector<long long>
+#define ss second
+#define ff first
+using namespace std;
+
+
+
+
+void solve()
+{
+
+}
+
+ 
+ 
+
+
+
+int main()
+{
+    fast;
+   vector<vector<int>> g(3, vector<int>(3, 1));
+
+    forn(i,0,3) {
+        forn(j,0,3) {
+            int n;
+            cin >> n;
+
+            
+            if(n%2 !=0){
+                g[i][j] ^=1;
+              
+                if (i > 0) g[i - 1][j] ^= 1; 
+                if (i < 2) g[i + 1][j] ^= 1; 
+                if (j > 0) g[i][j - 1] ^= 1; 
+                if (j < 2) g[i][j + 1] ^= 1; 
+            }
+        }
+    }
+
+
+     forn(i,0,3){
+            forn(j,0,3){
+                cout << g[i][j];
+            }
+            cout << endl;
+        }
+    
+  
+
+
+
+    return 0;
+
+
+}
